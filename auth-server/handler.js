1628ea9cbabae295ecc6f14b749261e1332ac008
Name-Javi <62718685+Name-Javi@users.noreply.github.com>
2021-04-22 18:38:33 -0500

    redirect adjustment in handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: [""],
+  redirect_uris: ["https://name-javi.github.io/meet/"],
   javascript_origins: ["https://name-javi.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
