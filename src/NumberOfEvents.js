48d38f66189227d77dcb55cd8bd539d2985dd7e0
Name-Javi <62718685+Name-Javi@users.noreply.github.com>
2021-04-23 14:51:10 -0500

    4.3 Task

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+    constructor() {
+        super();
+        this.state = {
+            eventsPerPage: 32
+        }
+    }
+
+    handleInputChange = (event) => {
+        this.setState({
+            eventsPerPage: event.target.value
+        });
+    }
+
+    render() {
+        return <div className='NumberOfEvents'>
+            <input 
+                type="number"
+                className="number"
+                placeholder="32"
+                value={this.state.eventsPerPage}
+                onChange={this.handleInputChange}
+
+            />
+        </div>
+    }
+}
+
+export default NumberOfEvents;
